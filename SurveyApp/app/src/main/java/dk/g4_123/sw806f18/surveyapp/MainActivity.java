5439d713a4878da242a0f10527f187f4a945778e
Unknown <august@aggisoft.dk>
2018-03-23 09:19:26 +0100

    Init Android

---
+++ SurveyApp/app/src/main/java/dk/g4_123/sw806f18/surveyapp/MainActivity.java
@@ -0,0 +1,13 @@
+package dk.g4_123.sw806f18.surveyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

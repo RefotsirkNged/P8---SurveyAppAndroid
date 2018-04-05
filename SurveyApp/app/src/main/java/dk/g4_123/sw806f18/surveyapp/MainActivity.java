33a3e15236c83ab7aebdf902b8a3cfc51f4a93a4
August Kørvell <august@aggisoft.dk>
2018-04-05 14:00:13 +0200

    Initial app

--- SurveyApp/app/src/main/java/dk/g4_123/sw806f18/surveyapp/MainActivity.java
+++ SurveyApp/app/src/main/java/dk/g4_123/sw806f18/surveyapp/MainActivity.java
@@ -1,13 +1,63 @@
 package dk.g4_123.sw806f18.surveyapp;
 
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    final static String url = "192.168.1.123:8081/api/survey/5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.web_view);
+    }
+
+    private void initWebView(){
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setDomStorageEnabled(true);
+        settings.setBuiltInZoomControls(false);
+        settings.setLoadWithOverviewMode(false);
+        settings.setUseWideViewPort(false);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+                new AlertDialog.Builder(view.getContext()).setTitle("JS Dialog")
+                        .setMessage(message).setPositiveButton(android.R.string.ok,
+                        new AlertDialog.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                result.confirm();
+                            }
+                        }).setCancelable(false).create().show();
+                return true;
+            }
+        });
+
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+            return;
+        }
+
+        super.onBackPressed();
     }
 }
